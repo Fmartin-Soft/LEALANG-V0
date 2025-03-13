880bab702c70759f2dbe3a74fc7363bc767ba0d3
Fmartin-Soft <finley.martin-6@students.plymouth.ac.uk>
2025-03-13 12:44:03 +0000

    starting database stuff

---
+++ LEALANG V0/MakeDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace LEALANG_V0
+{
+    internal class MakeDatabase
+    {
+        String db = "CREATE DATABASE userinfo ON PRIMARY"+"(NAME = "
+        SqlConnection con = new SqlConnection("Server=localhost;Integrated security=SSPI;database=master");
+        
+    }
+}
